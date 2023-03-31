992bda3579924f37ec2c17cb4a74eb0ba4e7a611
jsproton <johndavidsimmons@gmail.com>
2023-03-31 16:02:41 -0400

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
   title: 'perpetua.digital',
   author: 'John Simmons',
   headerTitle: 'perpetua.digital',
-  description: 'A blog stuff',
+  description: 'A blog about stuff',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
