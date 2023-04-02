c1f3401da47314d470af59e70f672baac1ac7878
jsproton <johndavidsimmons@gmail.com>
2023-04-02 13:31:18 -0400

    new post

--- .contentlayer/generated/Blog/_index.mjs
+++ .contentlayer/generated/Blog/_index.mjs
@@ -2,5 +2,6 @@
 
 import blog__adobeAnalyticsAlertsInMicrosoftTeamsMdx from './blog__adobe-analytics-alerts-in-microsoft-teams.mdx.json' assert { type: 'json' }
 import blog__collectingQueryParametersWithAmplitudeInAdobeLaunchMdx from './blog__collecting-query-parameters-with-amplitude-in-adobe-launch.mdx.json' assert { type: 'json' }
+import blog__usingDataFromAdobeAnalyticsInAdobeTargetOffersMdx from './blog__using-data-from-adobe-analytics-in-adobe-target-offers.mdx.json' assert { type: 'json' }
 
-export const allBlogs = [blog__adobeAnalyticsAlertsInMicrosoftTeamsMdx, blog__collectingQueryParametersWithAmplitudeInAdobeLaunchMdx]
+export const allBlogs = [blog__adobeAnalyticsAlertsInMicrosoftTeamsMdx, blog__collectingQueryParametersWithAmplitudeInAdobeLaunchMdx, blog__usingDataFromAdobeAnalyticsInAdobeTargetOffersMdx]
